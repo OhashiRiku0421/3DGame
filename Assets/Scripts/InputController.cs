3e432e437547d38a0345cde5f3f31b68805a7953
RK <riku.kuu0421@gmail.com>
2023-01-21 23:21:47 +0900

    [add]InputSystemがうまくいきません

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController
+{
+    ////private PlayerInputSystem _input;
+    //private PlayerInput _playerInput;
+
+    //public void Input()
+    //{
+    //    _playerInput.onActionTriggered += PlayerInput;
+    //}
+
+    //private void PlayerInput(InputAction.CallbackContext context)
+    //{
+    //    var test = context.ReadValue<Vector3>();
+    //}
+}
