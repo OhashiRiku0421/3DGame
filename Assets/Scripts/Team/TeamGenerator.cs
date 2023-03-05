1958f2b6adaa22c4628474df283f6db6dc918db2
RK <riku.kuu0421@gmail.com>
2023-03-05 23:44:14 +0900

    [update]チームジェネレータークラスを修正

--- Assets/Scripts/Team/TeamGenerator.cs
+++ Assets/Scripts/Team/TeamGenerator.cs
@@ -5,57 +5,36 @@ using UnityEngine;
 public class TeamGenerator : MonoBehaviour
 {
     [SerializeField, Tooltip("必要なチームの数")]
-    private int _teamNum = 10;
+    private int _teamNum = 2;
 
     [SerializeField]
-    private TeamObjectPool _teamPool = null;
+    private ObjectPool _teamPool = null;
 
-    public void Generator(Transform[] transforms, Transform playerPos)
-    {
-        for (int i = 0; i < _teamNum; i++)
-        {
-            TeamPop(transforms, playerPos);
-            //_teamPool.PoolPop(transforms);
-        }
-    }
-
-    /// <summary>
-    /// Poolのオブジェクトをどこで使うかを決めるメソッド
-    /// </summary>
-    private void TeamPop(Transform[] teamPos, Transform playerPos)
-    {
-        foreach (var n in teamPos)
-        {
-            if (!n.gameObject.activeSelf)
-            {
-                n.gameObject.SetActive(true);
-                //_teamPool.PoolPop(n, playerPos);
-                return;
-            }
-        }
-
-        Debug.LogError("基底より多く使おうとしています。");
-    }
+    private bool _isPop = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<PlayercCntroller>(out PlayercCntroller playercCntroller))
+        if (other.TryGetComponent<TeamController>(out TeamController playercCntroller) && !_isPop)
         {
-            Generator(playercCntroller.TeamPos, playercCntroller.transform);
-
+            _isPop = true;
+            StartCoroutine(PopInterval());
             RandomTransform(playercCntroller.RangeLeft,
-                playercCntroller.RangeRight,
-                playercCntroller.transform);
+                playercCntroller.RangeRight);
         }
     }
+    IEnumerator PopInterval()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _isPop = false;
+    }
 
-    private void RandomTransform(Transform rangeLeft, Transform RrangeRight, Transform playr)
+    private void RandomTransform(Transform rangeLeft, Transform rangeRight)
     {
         for (int i = 0; i < _teamNum; i++)
         {
-            var x = Random.Range(rangeLeft.position.x, RrangeRight.position.x);
-            var z = Random.Range(rangeLeft.position.z, RrangeRight.position.z);
-            _teamPool.PoolPop(new Vector3(x, 0, z), playr);
+            var x = Random.Range(rangeLeft.position.x, rangeRight.position.x);
+            var z = Random.Range(rangeLeft.position.z, rangeRight.position.z);
+            _teamPool.PoolPop(new Vector3(x, 0, z));
         }
     }
 }
