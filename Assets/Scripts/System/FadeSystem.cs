647d3b867e24bf543394dc5edb7cc326a9baee0c
RK <riku.kuu0421@gmail.com>
2023-03-05 23:40:58 +0900

    [add]Fade用のクラスを作成

---
+++ Assets/Scripts/System/FadeSystem.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Image _image;
+
+    void Start()
+    {
+        FadeIn();
+    }
+
+    /// <summary>
+    /// フェードアウト
+    /// </summary>
+    public void FadeOut(string sceneName)
+    {
+        _image.enabled = true;
+        _image.DOFade(1, 1.5f)
+            .SetDelay(0.5f)
+            //fadeoutが終わったら呼ばれる
+            .OnComplete(() => SceneManager.LoadScene(sceneName));
+    }
+
+    /// <summary>
+    /// フェードイン
+    /// </summary>
+    public void FadeIn()
+    {
+        _image.enabled = true;
+        _image.DOFade(0, 1.5f)
+        .SetDelay(0.5f)
+        .OnComplete(() => _image.enabled = false);
+    }
+}
